c8fff742b3a9299c5ba2aa7d9ebd7c4c514dc6d9
IshaanIG <160488678+IshaanIG@users.noreply.github.com>
2024-02-19 20:53:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-let timer;
-let seconds = 0;
-let reps = 1;
-
+// Initialize the audio element
 let audio = new Audio('beep.wav');
 
-function startTimer() {
-    if (timer) {
-        clearInterval(timer);
-    }
-
-    timer = setInterval(() => {
-        document.getElementById('timer-display').innerText = `Rep ${reps}: 
-Lift leg for ${seconds} seconds`;
-
-        if (seconds === 5) {
-            clearInterval(timer);
-            seconds = 0;
-            reps++;
-            document.getElementById('timer-display').innerText = `Rest for 
-2 seconds`;
-	audio.play();  // Play the beep sound
-            setTimeout(() => {
-                startTimer();
-            }, 2000);
-        } else {
-            seconds++;
-        }
-    }, 1000);
+// Function to play the beep sound
+function playBeep() {
+    audio.play();
 }
+
+// Function to control the leg lifting timer
+function legLiftTimer() {
+    // Initial beep to start the timer
+    playBeep();
+
+    // Loop to repeat the timer
+    setInterval(() => {
+        // Beep to lift the leg
+        playBeep();
+
+        // Wait for 5 seconds
+        setTimeout(() => {
+            // Beep to lower the leg
+            playBeep();
+        }, 5000); // 5000 milliseconds = 5 seconds
+
+        // Wait for 2 seconds (pause)
+        setTimeout(() => {
+            // Beep to signal the next leg lift
+            playBeep();
+        }, 2000); // 2000 milliseconds = 2 seconds
+    }, 7000); // 7000 milliseconds = 7 seconds
+}
+
+// Call the legLiftTimer function to start the timer
+legLiftTimer();
 
